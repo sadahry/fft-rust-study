519cd29749efdfbf007d0c5c9c59ba1f9d60ec28
sadahiroyoshi <sadahiroyoshi@gmail.com>
2022-01-08 14:38:58 +0900

    fix fs->size

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 use num::complex::Complex;
 use std::f64::consts::PI;
 
-pub fn dtft(frames: Vec<f64>, fs: i64) -> Vec<Complex<f64>> {
+pub fn dtft(frames: Vec<f64>, size: i64) -> Vec<Complex<f64>> {
     let mut rslt: Vec<Complex<f64>> = Vec::new();
-    // 1 <= f <= fs/2
-    for f in 1..=(fs / 2) {
+    // 1 <= f <= size/2
+    for f in 1..=(size / 2) {
         let mut sigma: Complex<f64> = Complex::new(0.0, 0.0);
         // 0 <= k <= N-1
         for (k, xk) in frames.iter().enumerate() {
-            // t = k / fs
-            let t: f64 = (k as f64) / (fs as f64);
+            // t = k / size
+            let t: f64 = (k as f64) / (size as f64);
             // ω = 2πf
             let w = 2.0 * PI * (f as f64);
             // e^(-iωt)
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use fft_rs::dtft;
 use std::f64::consts::PI;
 
+fn get_size(flame_len: i64) -> i64 {
+    let mut i: u32 = 0;
+    while flame_len > 2i64.pow(i) {
+        i += 1;
+    }
+    2i64.pow(i)
+}
+
 fn main() {
     // create sin_curve fn
-    let flame_len = 1000;
+    let flame_len = 400;
     let samplerate = 16000;
     let hz = 2000;
     let sin_curve: Vec<f64> = (0..flame_len)
@@ -11,8 +19,8 @@ fn main() {
         .map(|a| a.sin())
         .collect();
 
-    // ./target/release/fft_rs  2.85s user 0.02s system 97% cpu 2.938 total
-    let rslt = dtft(sin_curve, samplerate);
+    let size = get_size(flame_len);
+    let rslt = dtft(sin_curve, size);
 
     let (max_index, max) =
         rslt.iter()
@@ -24,7 +32,11 @@ fn main() {
                     (i_a, a)
                 }
             });
-    println!("rslt len: {:?}", rslt.len()); // = fs/2
-    println!("max rslt f: {:?}", max_index + 1); // 1 <= f <= fs/2
+    let max_hz: f64 = (((max_index + 1) * samplerate) as f64) / (size as f64);
+
+    println!("flame_len: {:?}", flame_len); // = 400;
+    println!("rslt len: {:?}", rslt.len()); // = 256 (= 512/2);
+    println!("max index: {:?}", max_index);
     println!("max rslt: {:?}", max);
+    println!("max hz: {:?}", max_hz); // = 2000
 }
